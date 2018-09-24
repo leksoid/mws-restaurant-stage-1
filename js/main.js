1b1af4ec92fecf0f863f66e91937bf1b67cbbe5e
leksoid <aliaksei.taran@gmail.com>
2018-09-24 00:36:21 -0400

    Added responsiveness to homepage and connected Map API token

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibGVrc29pZCIsImEiOiJjam1mcmxueHIwczh2M2p0OGF0dnM4dHk4In0.IGmT7Ro3-qPgENiNeggP_A',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
